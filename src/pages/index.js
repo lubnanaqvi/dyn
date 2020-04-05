cd33885867229944a8c9c876b1c1f341689f799c
unknown <lubnanaqvi11@gmail.com>
2020-04-05 18:05:11 -0400

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,39 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const list = data.allContentfulWhatsNew.edges.map((l, i) => (
+    <li key={i}>{l.node.heading}</li>
+  ))
+  console.log(list)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>A simple Gatsby-Contentful-Netlify Project</h1>
+      <p>Checking pipeline. And others</p>
+      <ul>{list}</ul>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allContentfulWhatsNew {
+      edges {
+        node {
+          heading
+        }
+      }
+    }
+  }
+`
